6af30b3a63b790f47de0a97e879902fb87529659
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-30 22:20:17 +0330

    Create ipinfo.php

---
+++ modules/ipinfo.php
@@ -0,0 +1,27 @@
+<?php
+
+function is_ip($string)
+{
+    $ip_pattern = '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/';
+    if (preg_match($ip_pattern, $string)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ip_info($ip)
+{
+    if (is_ip($ip) === false) {
+        $ip_address_array = dns_get_record($ip, DNS_A);
+        $randomKey = array_rand($ip_address_array);
+        $ip = $ip_address_array[$randomKey]["ip"];
+    }
+    $ipinfo = json_decode(
+        file_get_contents("https://api.country.is/" . $ip),
+        true
+    );
+    return $ipinfo;
+}
+
+?>
