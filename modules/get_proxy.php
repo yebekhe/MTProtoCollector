f2bbe80d3cf7136a08d255981d70bcd271a47c98
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-30 23:47:43 +0330

    Update get_proxy.php

--- modules/get_proxy.php
+++ modules/get_proxy.php
@@ -20,34 +20,53 @@ function getProxies($channel)
     return $prxs[2] ?: $in_prxs[2];
 }
 
-function parse_proxy($proxy, $name){
+function parse_proxy($proxy, $name)
+{
+    $proxy_array = [];
     $url = html_entity_decode($proxy);
     $parts = parse_url($proxy);
-    $query_string = str_replace("amp;", "", $parts['query']);
+    $query_string = str_replace("amp;", "", $parts["query"]);
     parse_str($query_string, $query_params);
-    foreach ($query_params as $key => $value) {
-        if (stripos($key, '@') !== false) { 
-            unset($query_params[$key]); // remove the old parameter
-            break; // exit the loop after processing the first parameter with @ symbol
+    if (
+        ping($query_params["server"], $query_params["port"]) !== "unavailable"
+    ) {
+        foreach ($query_params as $key => $value) {
+            if (stripos($key, "@") !== false) {
+                unset($query_params[$key]); // remove the old parameter
+                break; // exit the loop after processing the first parameter with @ symbol
+            }
         }
+        $ip_data = ip_info($query_params["server"]);
+        if (isset($ip_data["country"])) {
+            $location = $ip_info["country"];
+            $flag = getFlags($location);
+        } else {
+            $flag = "🚩";
+        }
+        $query_params["name"] = "@" . $name . "|" . $flag;
+        $proxy_array = $parts;
+        unset($proxy_array["query"]);
+        $proxy_array["query"] = $query_params;
     }
-    $query_params['name'] = $name;
-    $proxy_array = $parts;
-    unset($proxy_array['query']);
-    $proxy_array['query'] = $query_params;
-    
     return $proxy_array;
 }
 
-function proxy_array_maker($source){
+function proxy_array_maker($source)
+{
     $key_limit = count(getProxies($source)) - 3;
     $output = [];
-    foreach (getProxies($source) as $key => $proxy){
+    foreach (getProxies($source) as $key => $proxy) {
         if ($key >= $key_limit) {
             $proxy = "https://t.me/proxy" . $proxy;
-            $output[$key - $key_limit] = parse_proxy($proxy, $source);
-       }
-   }
-   return $output;
+            $data = parse_proxy($proxy, $source);
+            if ($data === []) {
+                null;
+            } else {
+                $output[$key - $key_limit] = $data;
+            }
+        }
+    }
+    return $output;
 }
+
 ?>
