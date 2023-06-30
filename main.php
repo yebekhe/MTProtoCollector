535eb57b9d164ce0cfb8d1180b1f53a5528acfd2
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-01 00:43:30 +0330

    Update main.php

--- main.php
+++ main.php
@@ -5,12 +5,8 @@ include "modules/config.php";
 $final_data = [];
 foreach ($sources as $source) {
     $final_data = array_merge($final_data, proxy_array_maker($source));
+    $final_output = remove_duplicate($final_data);
 }
 
-$proxy_subscription = "";
-foreach ($final_data as $proxy_data){
-  $proxy_subscription .= $proxy_data['scheme'] . "://" . $proxy_data['host'] . $proxy_data['path'] . "?server=" . $proxy_data['query']['server'] . "&port=" . $proxy_data['query']['port'] . "&secret=" . $proxy_data['query']['secret'] . "&" . $proxy_data['query']['name'] . "\n";
-}
-
-file_put_contents("proxy/mtproto", $proxy_subscription);
+file_put_contents("proxy/mtproto", json_encode($final_output, JSON_PRETTY_PRINT));
 ?>
